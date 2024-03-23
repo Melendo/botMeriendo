1d708477b87afa78737e52acd78ac36859ed11c4
Melendo <nachomelendo@gmail.com>
2024-03-23 13:48:45 +0100

    Arreglado bot.events y añadido proc message

--- bot.py
+++ bot.py
@@ -11,31 +11,44 @@ load_dotenv(".env")
 TOKEN = os.getenv("TOKEN")
 
 #Configuracion del bot y cliente
+descripcion = "Bot todopoderoso hijo de Odin y hermano de Prometeo"
 prefix = "!"
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
-bot = commands.Bot(command_prefix=prefix, intents=intents)
+bot = commands.Bot(command_prefix=prefix, intents=intents, description = descripcion)
+
+@bot.event
+async def on_message(message):
+    # do some extra stuff here
+    if message.author == bot.user:
+        return
+    
+    if message.author.bot:
+        return
+
+    await bot.process_commands(message)
 
 #Mensaje login por terminal
 @bot.event
 async def on_ready():
     print('We have logged in as {0}'.format(bot.user))
 
+@bot.event
+async def on_member_join(member):
+    await member.send(f'Bienvenido al server {member.name}, bonito nombre por cierto!')
+
+@bot.event
+async def on_member_remove(member):
+    await bot.get_channel(762326170799702016).send(f'{member.name} se ha ido a mi mi mi zzz... zzz... zzz...')
+
+@bot.command(name='ping' , help='pong?', category='Basico')
+async def pingpong(ctx):
+    await ctx.send('pong')
 
 #Funcion de saludar al usuario que ejecuta el comano
 @bot.command(name='hola', help='Saluda al usuario', category='Basico')
 async def saludar(ctx):
     await ctx.send(f'Hola {ctx.author.mention}!')
-
-@bot.event
-async def on_member_join(member):
-    # Aquí podrías enviar un mensaje de bienvenida personalizado
-    await member.send(f'Bienvenido al server {member.name}, bonito nombre por cierto!')
-
-@bot.event
-async def on_member_remove(member):
-    # Aquí podrías enviar un mensaje de despedida personalizado
-    await bot.get_channel(762326170799702016).send(f'{member.name} se ha ido a mi mi mi zzz... zzz... zzz...')
 
 #Funcion de busqueda en google que devuelve el primer enlace
 @bot.command(name='buscar', help='Busca en google y te devuelve la primera búsqueda', category='Utilidad')
@@ -65,24 +78,18 @@ async def buscar(ctx, *, query):
 @bot.command(name='rangoRL', help='introduciendo id epic devuelve el rango en 2s', category='Utilidad')
 
 async def rangoRL(ctx, *, query):
-    url = f"https://api.tracker.gg/api/v2/rocket-league/standard/profile/epic/{query}/sessions?"
+    url = f"https://public-api.tracker.gg/v2/rocket-league/standard/profile/epic/{query}/sessions?"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.1 Safari/537.36",
         "TRN-Api-Key": "c7d94c85-e536-479e-a775-f100438c41ed"
     }
 
     response = requests.get(url, headers=headers)
 
     print(response.status_code)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        result = soup.find('main').text
-        
-        if result:
-            
-            await ctx.send(f"El rango del jugador {query} es : {result}\nEnlace del tracker: {url}")
-        else:
-            await ctx.send(f"No se encontraron resultados.\nEnlace del tracker: {url}")
+    if response.status_code == 200: 
+        datos = response.json()
+        print(datos)
+        await ctx.send(f"El rango del jugador {query} es : \nEnlace del tracker: {url}")
     else:
         await ctx.send("Error al realizar la búsqueda.")
 
